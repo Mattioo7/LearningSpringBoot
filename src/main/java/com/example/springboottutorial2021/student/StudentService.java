0eefbba774bf6865b631d2382335d159d46687b8
Mattioo7 <mati.chudek@op.pl>
2022-05-10 20:21:15 +0200

    Added DELETE method

--- src/main/java/com/example/springboottutorial2021/student/StudentService.java
+++ src/main/java/com/example/springboottutorial2021/student/StudentService.java
@@ -3,10 +3,12 @@ package com.example.springboottutorial2021.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -31,4 +33,14 @@ public class StudentService {
 		}
 		studentRepository.save(student);
 	}
+
+	public void deleteStudent(Long studentId) {
+		boolean exists = studentRepository.existsById(studentId);
+
+		if (!exists) {
+			throw new IllegalStateException("student with id " + studentId + " does not exists");
+		}
+		studentRepository.deleteById(studentId);
+	}
+
 }
